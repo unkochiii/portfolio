6d49c141b012548e18d7ff7fa2227676600616a7
unkochi <anais.picaut@gmail.com>
2026-05-12 13:55:07 +0200

    uh

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Active le mode statique
-  distDir: "out", // ou 'dist' si tu veux
-  images: {
-    unoptimized: true, // Obligatoire sur hébergement mutualisé
-  },
-  // Si tu as des Server Actions → il faudra les supprimer ou les remplacer
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
